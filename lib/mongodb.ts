a1500d762acbee13cc034011f3449f120a8021a7
MariyaEfronY <mariyaefrony@gmail.com>
2025-09-26 19:27:57 +0530

    push7

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,7 +6,7 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
 }
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+let cached = (globalThis as any).mongoose || { conn: null, promise: null };
 
 export async function dbConnect() {
   if (cached.conn) return cached.conn;
